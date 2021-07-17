cf139d12f19e14663986f95a3a95c466f9258cbf
Mikhail Korobov <korobovms94@gmail.com>
2021-07-17 22:09:50 +0300

    Fixed clippy warnings

--- memops.rs
+++ memops.rs
@@ -2,7 +2,7 @@
 pub unsafe extern "C" fn memcpy(dst: *mut u8, src: *const u8, size: usize) -> *mut u8 {
     let mut i = 0usize;
     while i < size {
-        *dst.offset(i as isize) = *src.offset(i as isize);
+        *dst.add(i) = *src.add(i);
         i += 1;
     }
     dst
@@ -14,12 +14,12 @@ pub unsafe extern "C" fn memmove(dst: *mut u8, src: *const u8, size: usize) -> *
         let mut i = size;
         while i != 0 {
             i -= 1;
-            *dst.offset(i as isize) = *src.offset(i as isize);
+            *dst.add(i) = *src.add(i);
         }
     } else {
         let mut i = 0usize;
         while i < size {
-            *dst.offset(i as isize) = *src.offset(i as isize);
+            *dst.add(i) = *src.add(i);
             i += 1;
         }
     }
@@ -30,7 +30,7 @@ pub unsafe extern "C" fn memmove(dst: *mut u8, src: *const u8, size: usize) -> *
 pub unsafe extern "C" fn memset(mem: *mut u8, c: i32, size: usize) -> *mut u8 {
     let mut i = 0;
     while i < size {
-        *mem.offset(i as isize) = c as u8;
+        *mem.add(i) = c as u8;
         i += 1;
     }
     mem
@@ -40,8 +40,8 @@ pub unsafe extern "C" fn memset(mem: *mut u8, c: i32, size: usize) -> *mut u8 {
 pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
     let mut i = 0;
     while i < n {
-        let a = *s1.offset(i as isize);
-        let b = *s2.offset(i as isize);
+        let a = *s1.add(i);
+        let b = *s2.add(i);
         if a != b {
             return a as i32 - b as i32;
         }
